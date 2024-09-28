f2e5da5cfec61c0aea87d7f03413933698fa21d9
TylerSpaeth <tspaeth58@gmail.com>
2024-09-27 20:46:10 -0500

    fixed spacing issues

--- src/Window.hpp
+++ src/Window.hpp
@@ -5,21 +5,21 @@
 
 // Set the size of the OpenGL viewport when the window is resized
 void FramebufferSizeCallback(GLFWwindow* window, int width, int height) {
-	glViewport(0,0,width,height);
+  glViewport(0,0,width,height);
 }
 
 GLFWwindow* InitGLFWwindow(const unsigned int xDimension, const unsigned int yDimension) {
   glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
   GLFWwindow* window = glfwCreateWindow(xDimension,yDimension,"LearnOpenGL",NULL,NULL);
-	if(window == NULL) {
-		glfwTerminate();
-	}
+  if(window == NULL) {
+    glfwTerminate();
+  }
   // Make the window we created the current context on the current thread
-	glfwMakeContextCurrent(window);
+  glfwMakeContextCurrent(window);
 
   // Disables vsync
   glfwSwapInterval(0);
@@ -28,7 +28,7 @@ GLFWwindow* InitGLFWwindow(const unsigned int xDimension, const unsigned int yDi
   glfwSetWindowAspectRatio(window, xDimension, yDimension);
 
   // Register the viewport callback function to make the window resizable
-	glfwSetFramebufferSizeCallback(window, FramebufferSizeCallback);
+  glfwSetFramebufferSizeCallback(window, FramebufferSizeCallback);
   
   return window;
 }
